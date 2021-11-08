57de976de4e2879969f9f571109c1278c3af9f0e
adam-mgshgy <magashegya@gmail.com>
2021-11-08 20:23:29 +0100

    **App.component.html navbar** Fixes #7

--- WhereSweat/src/app/app-routing.module.ts
+++ WhereSweat/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginpageComponent } from './components/loginpage/loginpage.component';
+import { MainpageComponent } from './components/mainpage/mainpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: MainpageComponent},
+  {path: 'login', component: LoginpageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
