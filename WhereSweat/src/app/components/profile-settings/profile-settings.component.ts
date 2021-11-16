f4e3d95923cc75a13119b6a68e6685ea38f80f62
saravadkerti <svadkerti@gmail.com>
2021-11-16 09:34:30 +0100

    Form elements

--- WhereSweat/src/app/components/profile-settings/profile-settings.component.ts
+++ WhereSweat/src/app/components/profile-settings/profile-settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/models/user-model';
 
 @Component({
   selector: 'app-profile-settings',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileSettingsComponent implements OnInit {
 
+  user = new UserModel();
   constructor() { }
 
   ngOnInit(): void {
---
+++ WhereSweat/src/app/models/user-model.ts
@@ -0,0 +1,7 @@
+export class UserModel {
+    public id = 0;
+    public email = '';
+    public full_name = '';
+    public phone_number = '';
+    public trainer = false;
+}
