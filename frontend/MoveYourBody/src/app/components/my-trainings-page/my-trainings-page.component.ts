36b480054b9d8c80e07e53a74a34c6684b5fd2dc
adam-mgshgy <magashegya@gmail.com>
2021-12-01 14:46:55 +0100

    **My trainings page component** component created

---
+++ frontend/MoveYourBody/src/app/components/my-trainings-page/my-trainings-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-trainings-page',
+  templateUrl: './my-trainings-page.component.html',
+  styleUrls: ['./my-trainings-page.component.css']
+})
+export class MyTrainingsPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
