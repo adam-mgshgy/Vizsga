59fb794d002761a0a519783b58c04aff9dc82f2d
saravadkerti <svadkerti@gmail.com>
2021-12-05 16:44:31 +0100

    add session page

--- frontend/MoveYourBody/src/app/components/add-session-page/add-session-page.component.ts
+++ frontend/MoveYourBody/src/app/components/add-session-page/add-session-page.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-session-page.component.css']
 })
 export class AddSessionPageComponent implements OnInit {
-
+  mobile: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    if (window.innerWidth <= 800) {
+      this.mobile = true;
+    }
+    window.onresize = () => (this.mobile = window.innerWidth <= 991);
   }
 
 }
