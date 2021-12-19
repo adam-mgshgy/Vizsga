0a8c94b45665a803c2bb3aa4b6f8a1350629c578
saravadkerti <svadkerti@gmail.com>
2021-12-19 15:46:00 +0100

    **UserController**

---
+++ backend/MoveYourBody/MoveYourBody.Service/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MoveYourBody.Service.Models
+{
+    public class Login
+    {
+        [Required, StringLength(320)]
+        public string Email { get; set; }
+       
+        [Required, StringLength(255)]
+        public string Password { get; set; }
+    }
+}
