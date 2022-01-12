46bb8abe92192564c48470fcc9e5f0a7862d3870
saravadkerti <svadkerti@gmail.com>
2022-01-12 20:39:56 +0100

    **TrainingPageComponent connect with backend**

--- frontend/MoveYourBody/src/app/components/training-page/training-page.component.ts
+++ frontend/MoveYourBody/src/app/components/training-page/training-page.component.ts
@@ -4,6 +4,18 @@ import { TagModel } from 'src/app/models/tag-model';
 import { TrainingModel } from 'src/app/models/training-model';
 import { UserModel } from 'src/app/models/user-model';
 import { LocationModel } from 'src/app/models/location-model';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { TrainingService } from 'src/app/services/training.service';
+import { TagService } from 'src/app/services/tag.service';
+import { TagTrainingService } from 'src/app/services/tag-training.service';
+import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LoginService } from 'src/app/services/login.service';
+import { CategoryModel } from 'src/app/models/category-model';
+import { TagTrainingModel } from 'src/app/models/tag-training-model';
+import { TrainingSessionService } from 'src/app/services/training-session.service';
+import { LocationService } from 'src/app/services/location.service';
 
 @Component({
   selector: 'app-training-page',
@@ -11,102 +23,94 @@ import { LocationModel } from 'src/app/models/location-model';
   styleUrls: ['./training-page.component.css']
 })
 export class TrainingPageComponent implements OnInit {
+  id: Number;
+  user: UserModel;
 
-  public location: LocationModel = {
-    id: 1,
-    county_name: "Komárom-Esztergom megye",
-    city_name: "Bana",
-    address_name: "Kis Károly utca 11."
-  }
-  
-  public training: TrainingModel =
-    {
-      id: 0,
-      name: 'Nagyon hosszú nevű edzés',
-      category_id: 4,
-      trainer_id: 0,
-      min_member: 6,
-      max_member: 8,
-      description: 'Zenés TRX edzés Bana city központjában. Hozz magaddal törölközőt, váltócipőt és vizet! Várunk sok szeretettel!',
-      contact_phone: '0670123456'
-    }
-  public sessions: TrainingSessionModel[] = [
-    {
-      id: 1,
-      date: '2021.12.12. 15:30',
-      price: 1500,
-      minutes: 60,
-      training_id: 1,
-      address_name: '',
-      place_name: '',
-      location_id: 1
-    },
-    {
-      id: 1,
-      date: '2021.12.12. 15:30',
-      price: 1500,
-      minutes: 60,
-      training_id: 1,
-      address_name: '',
-      place_name: '',
-      location_id: 1
-    },
-    {
-      id: 1,
-      date: '2021.12.12. 15:30',
-      price: 1500,
-      minutes: 60,
-      training_id: 1,
-      address_name: '',
-      place_name: '',
-      location_id: 1
-    },
-    {
-      id: 2,
-      date: '2021.12.12. 16:30',
-      price: 1500,
-      minutes: 60,
-      training_id: 1,
-      address_name: '',
-      place_name: '',
-      location_id: 1
-    },
-    {
-      id: 3,
-      date: '2021.12.12. 17:30',
-      price: 1500,
-      minutes: 60,
-      training_id: 1,
-      address_name: '',
-      place_name: '',
-      location_id: 1
-    }
-  ]
-    public tags: TagModel[] = [
-      { id: 0, name: 'csoportos', colour: '#6610f2' },
-      { id: 1, name: 'erőnléti', colour: 'black' },
-      { id: 2, name: 'saját testsúlyos', colour: '#fd7e14' },
-      { id: 3, name: 'edzőterem', colour: 'red' },
-      { id: 4, name: 'zsírégető', colour: '#0dcaf0' },
-      { id: 5, name: 'személyi edzés', colour: '#0dcaf0' }
-    ];
-    user: UserModel = {
-      id: 1,
-      email: 'tesztelek@gmail.com',
-      full_name: 'Teszt Elek',
-      trainer: true,
-      password: "pwd",
-      phone_number: '+36701234678',
-      location_id: 1, 
-  }
+  public training: TrainingModel;
+  public trainer: UserModel;
+  public category: CategoryModel;
+  public sessions: TrainingSessionModel[];
+
+  categories: CategoryModel[];
+  locations: LocationModel[];
+  tagTraining: TagTrainingModel[] = [];
+  tags: TagModel[] = [];
+  subscription: Subscription;
   mobile: boolean = false;
-  constructor() { }
-
+  constructor(
+    private categoriesService: CategoriesService,
+    private trainingService: TrainingService,
+    private trainingSessionService: TrainingSessionService,
+    private loginService: LoginService, 
+    private tagService: TagService,
+    private tagTrainingService: TagTrainingService,
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private locationService: LocationService,
+  ) { }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
   ngOnInit(): void {
     if (window.innerWidth <= 800) {
       this.mobile = true;
     }
     window.onresize = () => (this.mobile = window.innerWidth <= 991);
-  }
+    this.subscription = this.loginService.currentUser.subscribe(user => this.user = user)
 
+    this.route.paramMap.subscribe((params) => {
+      this.id = Number(params.get('id'));
+    });
+    this.trainingService.getById(this.id).subscribe(
+      (result) => {
+        this.training = result;
+        this.userService.getUserById(this.training.trainer_id).subscribe(
+          (result) => {
+            this.trainer = result;
+            this.trainingSessionService.listByTrainingId(this.training.id).subscribe(
+              (result) => {
+                this.sessions = result;
+                console.log(this.sessions);
+                this.locationService.getLocations().subscribe(
+                  result => {
+                    this.locations = result;
+                  },
+                  error => console.log(error)
+                );
+              },
+              (error) => console.log(error)
+            );
+          },
+          (error) => console.log(error)
+        );
+      },
+      (error) => console.log(error)
+    );
+    
+    this.categoriesService.getCategories().subscribe(
+      (result) => {
+        this.categories = result;
+        var i = 0;
+        while (this.categories[i].id != this.training.category_id && i < this.categories.length) {
+          i++;
+        }
+        if (i < this.categories.length) {
+          this.category = this.categories[i];
+        }
+        this.tagTrainingService.getTags(this.category.id).subscribe(
+          (result) => {
+            this.tagTraining = result;
+            console.log(result);
+          },
+          (error) => console.log(error)
+        );
+        console.log(result)
+      },
+      (error) => console.log(error)
+    );
+    this.tagService.getTags().subscribe(
+      (result) => (this.tags = result),
+      (error) => console.log(error)
+    );
+  }
 }
