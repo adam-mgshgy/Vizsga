11034dc5a13ab233cddbb08c49db74e76ac97417
adam-mgshgy <magashegya@gmail.com>
2022-02-03 19:34:03 +0100

    upload and remove works on frontend

--- frontend/MoveYourBody/src/app/components/create-training-page/create-training-page.component.ts
+++ frontend/MoveYourBody/src/app/components/create-training-page/create-training-page.component.ts
@@ -52,7 +52,6 @@ export class CreateTrainingPageComponent implements OnInit {
     private tagTrainingService: TagTrainingService,
     private authenticationService: AuthenticationService,
     private router: Router
-
   ) {
     this.authenticationService.currentUser.subscribe((x) => (this.user = x));
   }
@@ -125,65 +124,39 @@ export class CreateTrainingPageComponent implements OnInit {
       (error) => console.log(error)
     );
   }
+  images: string[] = [];
   imageError: string;
   isImageSaved: boolean;
   cardImageBase64: string;
   fileChangeEvent(fileInput: any) {
+    console.log(fileInput.target.files);
     this.imageError = null;
     if (fileInput.target.files && fileInput.target.files[0]) {
-        // Size Filter Bytes
-        const max_size = 20971520;
-        const allowed_types = ['image/png', 'image/jpeg'];
-        const max_height = 2500;
-        const max_width = 2500;
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const imgBase64Path = e.target.result;
+        this.cardImageBase64 = imgBase64Path;
+        this.isImageSaved = true;
+        console.log(imgBase64Path);
+        this.images.push(this.cardImageBase64);
+      };
 
-        if (fileInput.target.files[0].size > max_size) {
-            this.imageError =
-                'Maximum size allowed is ' + max_size / 1000 + 'Mb';
-
-            return false;
-        }
-
-       
-        const reader = new FileReader();
-        reader.onload = (e: any) => {
-            const image = new Image();
-            image.src = e.target.result;
-            image.onload = rs => {
-                const img_height = rs.currentTarget['height'];
-                const img_width = rs.currentTarget['width'];
-
-                console.log(img_height, img_width);
-
-
-                if (img_height > max_height && img_width > max_width) {
-                    this.imageError =
-                        'Maximum dimentions allowed ' +
-                        max_height +
-                        '*' +
-                        max_width +
-                        'px';
-                    return false;
-                } else {
-                    const imgBase64Path = e.target.result;
-                    this.cardImageBase64 = imgBase64Path;
-                    this.isImageSaved = true;
-                    console.log(imgBase64Path)
-                    // this.previewImagePath = imgBase64Path;
-                }
-                return false;
-            };
-        };
-        
-        reader.readAsDataURL(fileInput.target.files[0]);
+      reader.readAsDataURL(fileInput.target.files[0]);
     }
     return false;
-}
+  }
+  selectedImage(image: string) {
+    for (const item of this.images) {
+      if (item == image) {
+        this.images.splice(this.images.indexOf(item), 1);
+      }
+    }
 
-removeImage() {
-    this.cardImageBase64 = null;
-    this.isImageSaved = false;
-}
+    if (this.images == []) {
+      this.cardImageBase64 = null;
+      this.isImageSaved = false;
+    }
+  }
 
   save() {
     if (this.errorCheck()) {
@@ -197,8 +170,7 @@ removeImage() {
               (result) => {
                 if (result.category_id != null || result.category_id != 0) {
                   this.errorMessage = 'Edzése sikeresen létrehozva!';
-                this.router.navigateByUrl('/mytrainings');
-
+                  this.router.navigateByUrl('/mytrainings');
                 }
               },
               (error) => console.log(error)
@@ -214,7 +186,6 @@ removeImage() {
               if (result.category_id != null || result.category_id != 0) {
                 this.errorMessage = 'Edzése sikeresen létrehozva!';
                 this.router.navigateByUrl('/mytrainings');
-
               }
 
               this.training.id = result.id;
@@ -249,7 +220,6 @@ removeImage() {
           (result) => {
             this.errorMessage = 'Edzése sikeresen frissítve!';
             this.router.navigateByUrl('/mytrainings');
-
           },
           (error) => console.log(error)
         );
