636d9bb52300a7f2e06d26ce9505bd14d779a8a3
adam-mgshgy <magashegya@gmail.com>
2022-02-03 09:31:11 +0100

    file upload on frontend

--- frontend/MoveYourBody/src/app/components/create-training-page/create-training-page.component.ts
+++ frontend/MoveYourBody/src/app/components/create-training-page/create-training-page.component.ts
@@ -125,6 +125,66 @@ export class CreateTrainingPageComponent implements OnInit {
       (error) => console.log(error)
     );
   }
+  imageError: string;
+  isImageSaved: boolean;
+  cardImageBase64: string;
+  fileChangeEvent(fileInput: any) {
+    this.imageError = null;
+    if (fileInput.target.files && fileInput.target.files[0]) {
+        // Size Filter Bytes
+        const max_size = 20971520;
+        const allowed_types = ['image/png', 'image/jpeg'];
+        const max_height = 2500;
+        const max_width = 2500;
+
+        if (fileInput.target.files[0].size > max_size) {
+            this.imageError =
+                'Maximum size allowed is ' + max_size / 1000 + 'Mb';
+
+            return false;
+        }
+
+       
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+            const image = new Image();
+            image.src = e.target.result;
+            image.onload = rs => {
+                const img_height = rs.currentTarget['height'];
+                const img_width = rs.currentTarget['width'];
+
+                console.log(img_height, img_width);
+
+
+                if (img_height > max_height && img_width > max_width) {
+                    this.imageError =
+                        'Maximum dimentions allowed ' +
+                        max_height +
+                        '*' +
+                        max_width +
+                        'px';
+                    return false;
+                } else {
+                    const imgBase64Path = e.target.result;
+                    this.cardImageBase64 = imgBase64Path;
+                    this.isImageSaved = true;
+                    console.log(imgBase64Path)
+                    // this.previewImagePath = imgBase64Path;
+                }
+                return false;
+            };
+        };
+        
+        reader.readAsDataURL(fileInput.target.files[0]);
+    }
+    return false;
+}
+
+removeImage() {
+    this.cardImageBase64 = null;
+    this.isImageSaved = false;
+}
+
   save() {
     if (this.errorCheck()) {
       if (this.create) {
