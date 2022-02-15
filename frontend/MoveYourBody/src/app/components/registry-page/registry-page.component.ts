7e3e7e81a9e61836d4fa9a5d53335e188b03040c
adam-mgshgy <magashegya@gmail.com>
2022-02-15 13:18:09 +0100

    session delete email sending works

--- frontend/MoveYourBody/src/app/components/registry-page/registry-page.component.ts
+++ frontend/MoveYourBody/src/app/components/registry-page/registry-page.component.ts
@@ -74,6 +74,7 @@ export class RegistryPageComponent implements OnInit {
         (error) => console.log(error)
       );
       this.newUser.id = 0;
+      this.newUser.imageId = 0;
       this.userService.Register(this.newUser).subscribe(
         (result) => {
           console.log(this.newUser);
