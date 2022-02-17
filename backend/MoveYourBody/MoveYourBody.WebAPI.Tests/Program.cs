8aebff7048420c0f374199e08b50059bf902d380
saravadkerti <svadkerti@gmail.com>
2022-02-17 17:28:05 +0100

    **Backend tests starter**  doesnt work

---
+++ backend/MoveYourBody/MoveYourBody.WebAPI.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoveYourBody.WebAPI.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
