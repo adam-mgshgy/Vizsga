5b1331a20a5e01146a192ea368d62e54f9a15090
adam-mgshgy <magashegya@gmail.com>
2022-02-21 08:52:23 +0100

    **Backend tests starter**

---
+++ backend/MoveYourBody/MoveYourBody.WebAPI.Tests/TestDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MoveYourBody.Service;
+using System;
+
+namespace MoveYourBody.WebAPI.Tests
+{
+    internal class TestDbContext : ApplicationDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase($"moveyourbody{Guid.NewGuid()}");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+        public static TestDbContext GenerateTestDbContext()
+        {
+            var context = new TestDbContext();
+            context.Database.EnsureCreated();
+            return context;
+        }
+    }
+}
