a722a99264dab60ed3682376ba01542562c8bec4
adam-mgshgy <magashegya@gmail.com>
2022-02-27 11:48:54 +0100

    **Categories tests**

--- backend/MoveYourBody/MoveYourBody.Frontend.Tests/CategoriesPageTest.cs
+++ backend/MoveYourBody/MoveYourBody.Frontend.Tests/CategoriesPageTest.cs
@@ -2,6 +2,7 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace MoveYourBody.Frontend.Tests
@@ -9,7 +10,7 @@ namespace MoveYourBody.Frontend.Tests
     public class CategoriesPageTest
     {
         [Fact]
-        public void NumberOfCategories()
+        public void TextOfCategories()
         {
             using (var driver = new ChromeDriver())
             {
@@ -18,27 +19,57 @@ namespace MoveYourBody.Frontend.Tests
                 driver.Manage().Window.Maximize();
                 driver.FindElement(By.Name("email")).SendKeys("jozsiedzo@email.com");
                 driver.FindElement(By.Name("password")).SendKeys("jozsi");
-
-                System.Threading.Thread.Sleep(5000);
+                driver.FindElement(By.CssSelector(".btnSubmit")).Click();
 
                 driver.Navigate().GoToUrl("http://localhost:4200/categories");
                 driver.Manage().Window.Maximize();
 
-                System.Threading.Thread.Sleep(5000);
-                
+                System.Threading.Thread.Sleep(300);
+                driver.FindElement(By.XPath("(//a[@id='catRef']/span)[144]"));
+                Assert.Equal("Jóga", driver.FindElement(By.XPath("(//a[@id='catRef']/span)[144]")).Text);
+                Assert.Equal(12, driver.FindElements(By.XPath("//img[@id='catImg']")).Count);
 
-                //driver.FindElement(By.XPath("//span[12]/img")).WaitForDisplayed(5);
-  
+               
+            }
+        }
+        [Fact]
+        public void CategoriesDropdown()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:4200/login");
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Name("email")).SendKeys("jozsiedzo@email.com");
+                driver.FindElement(By.Name("password")).SendKeys("jozsi");
+                driver.FindElement(By.CssSelector(".btnSubmit")).Click();
 
-               // Assert.Equal("Jóga", driver.FindElement(By.XPath("span:nth-child(12)>#catImg")).Text);
-                //driver.FindElement(By.CssSelector(".btnSubmit")).Click();
-                //driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).WaitForDisplayed(500);
+                System.Threading.Thread.Sleep(300);
 
+                driver.FindElement(By.XPath("//a[contains(text(),'Kategóriák')]")).Click();
+                System.Threading.Thread.Sleep(30);
+                Assert.Equal(12, driver.FindElements(By.XPath("//img[@id='catImg']")).Count);
+            }
+        }
+        [Fact]
+        public void CheckUrl()
+        {
+            using (var driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:4200/login");
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Name("email")).SendKeys("jozsiedzo@email.com");
+                driver.FindElement(By.Name("password")).SendKeys("jozsi");
+                driver.FindElement(By.CssSelector(".btnSubmit")).Click();
 
-                //Assert.Equal("Edzõ József", driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).Text);
-                //driver.FindElement(By.LinkText("Edzõ József")).Click();
-                //driver.FindElement(By.LinkText("Kijelentkezés")).Click();
+                driver.Navigate().GoToUrl("http://localhost:4200/categories");
+                driver.Manage().Window.Maximize();
 
+                System.Threading.Thread.Sleep(300);
+
+                driver.FindElement(By.XPath("(//a[@id='catRef']/span)[12]")).Click();
+                System.Threading.Thread.Sleep(30);
+                string url = driver.Url;
+                Assert.Equal("http://localhost:4200/trainings/category/1", url);
             }
         }
     }
