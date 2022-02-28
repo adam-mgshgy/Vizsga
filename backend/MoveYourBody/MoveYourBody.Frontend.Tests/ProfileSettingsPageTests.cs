9e5b4435dd16e01d9d606d61d3f3e462b88f470d
saravadkerti <svadkerti@gmail.com>
2022-02-28 10:15:35 +0100

    finished

--- backend/MoveYourBody/MoveYourBody.Frontend.Tests/ProfileSettingsPageTests.cs
+++ backend/MoveYourBody/MoveYourBody.Frontend.Tests/ProfileSettingsPageTests.cs
@@ -19,18 +19,16 @@ namespace MoveYourBody.Frontend.Tests
                 driver.FindElement(By.Name("email")).SendKeys("jozsiedzo@email.com");
                 driver.FindElement(By.Name("password")).SendKeys("jozsi");
                 driver.FindElement(By.CssSelector(".btnSubmit")).Click();
-                System.Threading.Thread.Sleep(300);
 
-                driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).WaitForDisplayed(5);
+                By.XPath("//ul[@id='userDropdown']/li/a").WaitForExists(driver, 25);
 
                 Assert.Equal("Edzõ József", driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).Text);
 
                 driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).Click();
-                System.Threading.Thread.Sleep(300);
-
-                driver.FindElement(By.XPath("//a[contains(@href, '/profile')]")).WaitForDisplayed(5);
+                By.XPath("//a[contains(@href, '/profile')]").WaitForExists(driver, 5);
                 driver.FindElement(By.XPath("//a[contains(@href, '/profile')]")).Click();
-                System.Threading.Thread.Sleep(300);
+                By.ClassName("container").WaitForExists(driver, 5);
+
 
                 Assert.Equal("Edzõ József", driver.FindElement(By.Id("username")).GetAttribute("value"));
                 Assert.Equal("jozsiedzo@email.com", driver.FindElement(By.Id("email")).GetAttribute("value"));
@@ -53,8 +51,7 @@ namespace MoveYourBody.Frontend.Tests
                 driver.FindElement(By.Name("email")).SendKeys("jozsiedzo@email.com");
                 driver.FindElement(By.Name("password")).SendKeys("jozsi");
                 driver.FindElement(By.CssSelector(".btnSubmit")).Click();
-                //driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).WaitForDisplayed(50);
-                System.Threading.Thread.Sleep(300);
+                By.XPath("//ul[@id='userDropdown']/li/a").WaitForExists(driver, 25);
 
                 Assert.Equal("Edzõ József", driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).Text);
 
@@ -62,22 +59,18 @@ namespace MoveYourBody.Frontend.Tests
                 driver.FindElement(By.XPath("//a[contains(@href, '/profile')]")).WaitForDisplayed(5);
                 driver.FindElement(By.XPath("//a[contains(@href, '/profile')]")).Click();
 
-                System.Threading.Thread.Sleep(300);
-
+                By.ClassName("container").WaitForExists(driver, 25);
                 Assert.Equal("Jelszó módosítása", driver.FindElement(By.Id("passwordButton")).Text);
                 driver.FindElement(By.Id("passwordButton")).Click();
-                System.Threading.Thread.Sleep(300);
+               
 
                 driver.FindElement(By.Id("cancelbtn")).WaitForDisplayed(5);
                 Assert.Equal("X", driver.FindElement(By.Id("cancelbtn")).Text);
-                System.Threading.Thread.Sleep(300);
 
-                //driver.FindElement(By.Name("password")).WaitForDisplayed(5);
                 Assert.Equal("", driver.FindElement(By.Name("password")).Text);
 
                 driver.FindElement(By.Name("password2")).WaitForDisplayed(5);
                 Assert.Equal("", driver.FindElement(By.Name("password2")).Text);
-
 
                 //logout
                 driver.FindElement(By.LinkText("Edzõ József")).Click();
@@ -95,14 +88,13 @@ namespace MoveYourBody.Frontend.Tests
                 driver.FindElement(By.Name("email")).SendKeys("jozsiedzo@email.com");
                 driver.FindElement(By.Name("password")).SendKeys("jozsi");
                 driver.FindElement(By.CssSelector(".btnSubmit")).Click();
-                driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).WaitForDisplayed(5);
-
+                By.XPath("//ul[@id='userDropdown']/li/a").WaitForExists(driver, 25);
                 Assert.Equal("Edzõ József", driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).Text);
 
                 driver.FindElement(By.XPath("//ul[@id='userDropdown']/li/a")).Click();
                 driver.FindElement(By.XPath("//a[contains(@href, '/profile')]")).WaitForDisplayed(5);
                 driver.FindElement(By.XPath("//a[contains(@href, '/profile')]")).Click();
-                driver.FindElement(By.ClassName("container")).WaitForDisplayed(20);
+                By.ClassName("container").WaitForExists(driver, 5);
 
                 driver.FindElement(By.Id("cancelSubscriptionButton")).WaitForDisplayed(5);
                 Assert.Equal("Edzõség lemondása", driver.FindElement(By.Id("cancelSubscriptionButton")).Text);
